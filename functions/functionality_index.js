569a27ba864b952534785563e22ca103f9325de6
Gerard_Irao <gerardpaulirao@gmail.com>
2024-12-28 17:56:08 +0800

    insert new pics 20

--- functions/functionality_index.js
+++ functions/functionality_index.js
@@ -5,7 +5,7 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
-let target_date = new Date("January 31 2025 13:00:00").getTime()
+let target_date = new Date("January 31 2025 13:00:00").getTime();
 
 function timer ()
 {
@@ -22,49 +22,49 @@ function timer ()
 
     if (calculated_days < 10)
     {
-        days.innerHTML = "0" + calculated_days
+        days.innerHTML = "0" + calculated_days;
 
     } 
     else
     {
-        days.innerHTML = calculated_days
+        days.innerHTML = calculated_days;
     }
 
     if (calculated_hrs < 10)
     {
-        hours.innerHTML = "0" + calculated_hrs
+        hours.innerHTML = "0" + calculated_hrs;
 
     } 
     else
     {
-        hours.innerHTML =  calculated_hrs
+        hours.innerHTML =  calculated_hrs;
     }
 
     if (calculated_min < 10)
     {
-        minutes.innerHTML = "0" + calculated_min
+        minutes.innerHTML = "0" + calculated_min;
 
     } 
     else
     {
-        minutes.innerHTML =  calculated_min
+        minutes.innerHTML =  calculated_min;
     }
 
     if (calculated_sec <10)
     {
-        seconds.innerHTML = "0" + calculated_sec
+        seconds.innerHTML = "0" + calculated_sec;
     }
     else
     {
-        seconds.innerHTML = calculated_sec
+        seconds.innerHTML = calculated_sec;
     }
 
     if (time_diff < 0)
     {
-        days.innerHTML = "00"
-        hours.innerHTML = "00"
-        minutes.innerHTML = "00" 
-        seconds.innerHTML = "00" 
+        days.innerHTML = "00";
+        hours.innerHTML = "00";
+        minutes.innerHTML = "00"; 
+        seconds.innerHTML = "00"; 
     }
 
 }
